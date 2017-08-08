67ce6b7d28af4f8831af712975a90bce94585722
thirubala <30747201+thirubala@users.noreply.github.com>
2017-08-09 02:56:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName',function(req,res){
-    //articleName = articleOne/Two/Three through a built-in functionality of the APIs included
+    //articleName = articleOne/Two/Three through a built-in functionality of the API express included
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
