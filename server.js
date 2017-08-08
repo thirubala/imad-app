ca442d10430c7636784c4dc57c7e7bb9c03cd00e
thirubala <30747201+thirubala@users.noreply.github.com>
2017-08-09 02:30:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,45 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+  title: 'Article One | Thiru',
+  heading: 'Harry Potter',
+  date: 'Aug 9,2017',
+  Content: '<p>This page will contain Harry Potter updates, my first British obsession.</p>'
+};
+
+function createTemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    var htmlTemplate =`
+    <html>
+     <head>
+        <title>${title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+         <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div> 
+                    <a href="/">Home</a>
+                </div>
+                <div>
+                    <h2>${heading}</h2>
+                </div>
+                <div>
+                   ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
+            </div>
+            <br>
+        </body>  
+    </html>`; 
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
