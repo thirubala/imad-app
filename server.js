2036ead6865174f6aa176465222939a2de3b358f
thirubala <30747201+thirubala@users.noreply.github.com>
2017-08-09 00:25:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-    res.send("Article one coming soon...");
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two',function(req,res){
-    res.send("Article two coming soon...");
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three',function(req,res){
-    res.send("Article three coming soon...");
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 
