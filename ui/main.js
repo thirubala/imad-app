bccb9d52e1842d237ac40d85ee8f208bdcfe1f41
thirubala <30747201+thirubala@users.noreply.github.com>
2017-08-11 03:31:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,7 +32,7 @@ var comment = userInput.value;
 var submit = document.getElementById('submit');
 submit.onclick = function(){
     
-    var names = ['name1','name2','name3'];
+    var names = ['cmnt1','cmnt2','cmnt3'];
     var list = '';
     for(var i =0;i < names.length; i++){
         list += '<li>'+names[i]+'</li>';
