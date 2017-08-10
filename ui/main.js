8efcc4a5034475a606eff22898e0ce394622db88
thirubala <30747201+thirubala@users.noreply.github.com>
2017-08-11 03:48:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,116 +1,65 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
+var button = document.getElementById('submit');
 
-var app = express();
-app.use(morgan('combined'));
-
-var articles = {
-    'article-one': {
-      title: 'Article One | Thiru',
-      heading: 'Harry Potter',
-      date: 'Aug 9,2017',
-      content: '<p>This page will contain Harry Potter updates, my first British obsession.</p>'
-},
-    'article-two': {
-      title: 'Article Two | Thiru',
-      heading: 'Doctor Who',
-      date: 'Aug 19,2017',
-      content: '<p>This page will contain Doctor Who updates, my second British obsession.</p>'
-},
-    'article-three': {
-      title: 'Article Three | Thiru',
-      heading: 'Sherlock',
-      date: 'Aug 29,2017',
-      content: `<p>This page will contain Sherlock updates, my second British obsession.
-                Actually maybe not obsession in this case, more like appreciation.</p>`
-}
+button.onclick = function(){
+  
+  //creating an API reqeust object
+  var request = new XMLHttpRequest();
+  
+  //the change of state of the page load
+  request.onreadystatechange = function(){
+      
+      //if the reqeust is comepleted
+      if(request.readyState === XMLHttpRequest.DONE){
+          //if request is successful
+          if(request.status === 200){
+              
+              var names = ['cmnt1','cmnt2','cmnt3'];
+              var list = '';
+              for(var i =0;i < names.length; i++){
+              list += '<li>'+names[i]+'</li>';
+    }
+    var ul = document.getElementById('commentlist');
+    ul.innerHTML = list();
+          }
+      }
+  };
+  
+  //making the request
+  
+  request.open('GET','http://thirubala3171.imad.hasura-app.io/counter',true);
+  request.send(null);
+  
 };
 
-function createTemplate(data){
-    var title = data.title;
-    var heading = data.heading;
-    var date = data.date;
-    var content = data.content;
-    var htmlTemplate =`
-    <html>
-     <head>
-        <title>${title}</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-         <link href="/ui/style.css" rel="stylesheet" />
-        </head>
-        <body>
-            <div class="container">
-                <div> 
-                    <a href="/">Home</a>
-                </div>
-                <div>
-                    <h2>${heading}</h2>
-                </div>
-                <div>
-                   ${date}
-                </div>
-                <div>
-                    ${content}
-                </div>
-            </div>
-            <br>
-        </body>
-    </html>`; 
-    return htmlTemplate;
+var userInput = document.getElementById('user_text');
+var comment = userInput.value; 
+var submit = document.getElementById('submit');
+submit.onclick = function(){
+    
+    var names = ['cmnt1','cmnt2','cmnt3'];
+    var list = '';
+    for(var i =0;i < names.length; i++){
+        list += '<li>'+names[i]+'</li>';
+    }
+    var ul = document.getElementById('commentlist');
+    ul.innerHTML = list;
+};
+
+/*console.log('Loaded!');
+//Change the page text
+var text=document.getElementById('main-text');
+text.onclick = function(){
+  text.innerHTML = 'How was my first trick?'; 
+};
+//Move the image across the screen
+var img=document.getElementById('gif');
+var marginLeft = 0;
+//a user defined function to move the object by altering the left margin
+function moveRight(){
+ marginLeft = marginLeft + 5;
+ img.style.marginLeft = marginLeft + 'px';
+ 
 }
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-var counter = 0;
-app.get('/counter',function(req,res){
-    counter = counter+1;
-    res.send(counter.toString());
-});
-
-var names = [];
-app.get('/submit-name',function(req,res){
-   
-   var name = req.query.name;
-   
-   names.push(name);
-   
-   res.send(JSON.stringify(names));
-});
-
-app.get('/:articleName',function(req,res){
-    //articleName = articleOne/Two/Three through a built-in functionality of the API express included
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
-});
-
-app.get('/article-two',function(req,res){
-    res.send(createTemplate(articleTwo));
-});
-
-app.get('/article-three',function(req,res){
-    res.send(createTemplate(articleThree));
-});
-
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/letsgetstarted.gif', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'letsgetstarted.gif'));
-});
-
-app.get('/ui/main.js',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','main.js'));
-
-});
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+img.onclick = function(){
+    setInterval(moveRight,10);
+};*/
