5fd03b52ef82b5bf1363ecfb08b8d05fd5d19582
thirubala <30747201+thirubala@users.noreply.github.com>
2017-08-11 03:39:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,9 +71,9 @@ app.get('/counter',function(req,res){
 });
 
 var names = [];
-app.get('/submit-name/:name',function(req,res){
+app.get('/submit-name',function(req,res){
    
-   var name = req.params.name;
+   var name = req.query.name;
    
    names.push(name);
    
