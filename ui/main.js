5356942da75ff7fc825678fc7fc539553d4e3217
thirubala <30747201+thirubala@users.noreply.github.com>
2017-08-11 00:16:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,15 @@ var text=document.getElementById('main-text');
 text.onclick = function(){
   text.innerHTML = 'How was my first trick?'; 
 };
+
+//Move the image across the screen
+var img=document.getElementById('gif');
+var marginleft = 0;
+//a user defined function to move the object by altering the left margin
+function moveRight(){
+ marginLeft = marginLeft + 5;
+ img.style.marginLeft = marginLeft + 'px';
+ 
+}
+
+img.onclick = setInterval(moveRight,50);
