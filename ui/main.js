2fc6d2c2b7499739043c254e217ec4c3953bab76
thirubala <30747201+thirubala@users.noreply.github.com>
2017-08-11 03:53:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -46,6 +46,7 @@ submit.onclick = function(){
           //if request is successful
           if(request.status === 200){    
                 var names = request.responseText;
+                names = JSON.parse(names);
                 var list = '';
                 for(var i =0;i < names.length; i++){
                 list += '<li>'+names[i]+'</li>';
