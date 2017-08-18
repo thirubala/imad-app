10720f22c9331aee48790dd17328f47df9667eee
thirubala <30747201+thirubala@users.noreply.github.com>
2017-08-18 23:56:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var config = {
   database: 'thirubala3171',
   host: 'http://db.imad.hasura-app.io',
   port: '5432',
-  password : process.env.DB-PASSWORD
+  password : process.env.DB_PASSWORD
 };
 
 var app = express();
